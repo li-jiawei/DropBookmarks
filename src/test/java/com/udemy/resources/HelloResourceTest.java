b0a1a980baa1d7567d2242731a84c4c66e04618d
li-jiawei <li.jiawei@husky.neu.edu>
2017-07-14 00:18:06 -0400

    Create a unit test

---
+++ src/test/java/com/udemy/resources/HelloResourceTest.java
@@ -0,0 +1,18 @@
+package com.udemy.resources;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jiawei on 7/14/17.
+ */
+public class HelloResourceTest {
+    @Test
+    @Ignore
+    public void getGreeting() throws Exception {
+        fail("Not implemented");
+    }
+
+}
