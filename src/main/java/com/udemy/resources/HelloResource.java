d867ab6a6ee025a6ccf3839878703e56ce9ca597
li-jiawei <li.jiawei@husky.neu.edu>
2017-07-14 15:33:21 -0400

    Securing a method, asking authentication when requesting for that method

--- src/main/java/com/udemy/resources/HelloResource.java
+++ src/main/java/com/udemy/resources/HelloResource.java
@@ -1,5 +1,8 @@
 package com.udemy.resources;
 
+import com.udemy.core.User;
+import io.dropwizard.auth.Auth;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -15,5 +18,12 @@ public class HelloResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String getGreeting() {
         return "Hello world";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/secured")
+    public String getSecuredGreeting(@Auth User user) {
+        return "Hello secured world";
     }
 }
