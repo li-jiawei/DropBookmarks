4d45718d9e35e5d1cefb52d694b32368d0c77f1e
li-jiawei <li.jiawei@husky.neu.edu>
2017-07-15 10:11:02 -0400

    Adding a YAML configuration file add password file in authenticator pass configuration password in application main method to authenticator

--- src/main/java/com/udemy/DropBookmarksApplication.java
+++ src/main/java/com/udemy/DropBookmarksApplication.java
@@ -34,7 +34,7 @@ public class DropBookmarksApplication extends Application<DropBookmarksConfigura
         environment.jersey().register(
                 AuthFactory.binder(
                         new BasicAuthFactory<>(
-                                new HelloAuthenticator(),
+                                new HelloAuthenticator(configuration.getPassword()),
                                 "SECURITY REALM",
                                 User.class
                         )
--- src/main/java/com/udemy/auth/HelloAuthenticator.java
+++ src/main/java/com/udemy/auth/HelloAuthenticator.java
@@ -10,12 +10,18 @@ import io.dropwizard.auth.basic.BasicCredentials;
  * Created by Jiawei on 7/14/17.
  */
 public class HelloAuthenticator implements Authenticator<BasicCredentials, User>{
+    private String password;
+
+    public HelloAuthenticator(String password) {
+        this.password = password;
+    }
+
     @Override
     public Optional<User> authenticate(BasicCredentials basicCredentials) throws AuthenticationException {
         // basiCredenticals contains password, then query database
         // we hard code here
         // if found it, return the user wrapped by Optional
-        if ("p@ssw0rd".equals(basicCredentials.getPassword())) {
+        if (password.equals(basicCredentials.getPassword())) {
             return Optional.of(new User());
         } else {
             return Optional.absent();
